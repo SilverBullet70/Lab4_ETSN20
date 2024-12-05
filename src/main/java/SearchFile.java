d06a94c63b1cdab47566e9cbe344e941f8276dda
Angela Salem <angel.salem.008@gmail.com>
2024-12-05 14:27:38 +0100

    fixed issues after review

--- src/main/java/SearchFile.java
+++ src/main/java/SearchFile.java
@@ -1,38 +1,41 @@
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class SearchFile {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		try {
 
-			Scanner input = new Scanner(System.in);
-			String[] command = input.nextLine().split(" ");
+			BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+			String[] command = input.readLine().split(" ");
 
 			if (!command[0].equals("search"))
 				return;
 
 			String keyword = command[1];
-			String filename = command[2];
-			
-			
-			
-			File file = new File("src/main/resources/" + filename);
-			Scanner sc = new Scanner(file);
+			String filename = command[command.length - 1];
 
-			boolean flag = false;
-			while (sc.hasNextLine()) {
-				String line = sc.nextLine();
-				if (line.contains(keyword)) {
-					System.out.println(line);
-					flag = true;
-				}
+			BufferedReader sc = new BufferedReader(new FileReader("src/main/resources/" + filename));
+
+			StringBuilder sb = new StringBuilder();
+			String line = sc.readLine();
+			while (line != null) {
+
+				if (line.contains(keyword))
+					sb.append(line + "\n");
+				line = sc.readLine();
 			}
-			
-			if(!flag)
+
+			if (sb.toString().equals("") || sb.toString() == (null))
 				System.out.println(keyword + " Not Found in " + filename + ".");
-			
+			else
+				System.out.println(sb.toString());
+
 			sc.close();
 		} catch (FileNotFoundException e) {
 			System.err.println("File Not Found.");
